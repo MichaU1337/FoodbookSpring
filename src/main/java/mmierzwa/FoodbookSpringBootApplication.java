001edea5de7e7213d468c221fc2d195fba018a61
MichaU1337 <poczta.michau@gmail.com>
2022-09-26 20:26:20 +0200

    Initial commit. Add MySQL and MailDev config in application.properties.

---
+++ src/main/java/mmierzwa/FoodbookSpringBootApplication.java
@@ -0,0 +1,11 @@
+package mmierzwa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodbookSpringBootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FoodbookSpringBootApplication.class, args);
+    }
+}
